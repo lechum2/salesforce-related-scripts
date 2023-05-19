7680e746a14b83966199083e7eb126a853ef4bb4
Sebastian Lech <lechum2@gmail.com>
2023-05-19 15:53:25 +0200

    Parametrize metadata type to clear

--- removePackagesFromMeta/index.js
+++ removePackagesFromMeta/index.js
@@ -2,7 +2,10 @@ import { glob } from 'glob';
 import { readFileSync, writeFileSync } from 'fs';
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 
-const files = await glob('../../nyk/core/force-app/main/default/classes/**/*meta.xml', { stat: true, withFileTypes: true });
+const META_NODE_NAME = 'ApexTrigger';
+const META_PATH_NAME = 'triggers';
+
+const files = await glob(`../../nyk/core/force-app/main/default/${META_PATH_NAME}/**/*meta.xml`, { stat: true, withFileTypes: true });
 const parser = new XMLParser({
     ignoreAttributes: false
 });
@@ -12,11 +15,11 @@ for (const file of files) {
         continue;
     }
     let json = parser.parse(xmlFile);
-    delete json['ApexClass']['packageVersions'];
+    delete json[META_NODE_NAME]['packageVersions'];
     delete json['?xml'];
-    json['ApexClass']['apiVersion'] = Number.parseFloat(json['ApexClass']['apiVersion']).toFixed(1);
+    json[META_NODE_NAME]['apiVersion'] = Number.parseFloat(json[META_NODE_NAME]['apiVersion']).toFixed(1);
     const builder = new XMLBuilder({
-        arrayNodeName: 'ApexClass',
+        arrayNodeName: META_NODE_NAME,
         format: true,
         indentBy: '    ',
         ignoreAttributes: false,
