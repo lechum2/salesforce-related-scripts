80fdc3d79f77a5dd86655fe84f2230a1fc5be34d
Sebastian Lech <lechum2@gmail.com>
2023-05-19 12:07:30 +0200

    First version of working script for apex classes meta

--- removePackagesFromMeta/index.js
+++ removePackagesFromMeta/index.js
@@ -1,9 +1,11 @@
 import { glob } from 'glob';
-import { readFileSync } from 'fs';
-import { XMLParser } from 'fast-xml-parser';
+import { readFileSync, writeFileSync } from 'fs';
+import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 
-const files = await glob('../../core/force-app/main/default/classes/**/*meta.xml', { stat: true, withFileTypes: true });
-const parser = new XMLParser();
+const files = await glob('../../nyk/core/force-app/main/default/classes/**/*meta.xml', { stat: true, withFileTypes: true });
+const parser = new XMLParser({
+    ignoreAttributes: false
+});
 for (const file of files) {
     const xmlFile = readFileSync(file.fullpath(), 'utf8');
     if (!xmlFile.includes('<packageVersions>')) {
@@ -11,5 +13,16 @@ for (const file of files) {
     }
     let json = parser.parse(xmlFile);
     delete json['ApexClass']['packageVersions'];
-    console.log(json);
+    delete json['?xml'];
+    json['ApexClass']['apiVersion'] = Number.parseFloat(json['ApexClass']['apiVersion']).toFixed(1);
+    const builder = new XMLBuilder({
+        arrayNodeName: 'ApexClass',
+        format: true,
+        indentBy: '    ',
+        ignoreAttributes: false,
+    });
+    const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+${builder.build(json)}`;
+    console.log(xmlContent);
+    writeFileSync(file.fullpath(), xmlContent);
 }
