42cdc23cd1a13e9cb20ad9fa1a38227984711fc0
Sebastian Lech <y2dx@nykredit.dk>
2023-09-13 16:24:35 +0200

    Get list of packages with versions from sf

--- comparePackageVersions/index.js
+++ comparePackageVersions/index.js
@@ -1,11 +1,26 @@
 'use strict';
 
-const { spawnSync } = require( 'child_process' );
-const ls = spawnSync( 'ls', [ '-lh', '/usr' ] );
+const { execSync } = require( 'child_process' );
 
-console.log( `stderr: ${ ls.stderr.toString() }` );
-console.log( `stdout: ${ ls.stdout.toString() }` );
+const orgFromAlias = process.argv[2];
+const orgToAlias = process.argv[3];
+
+const project_path = 'C:\\Users\\Y2DX\\workspace\\core';
 
 function getInstalledPackages(orgAlias) {
-    return `sf package installed list --target-org ${orgAlias} --json`;
+    const orgFromPackages = execSync(`sf package installed list --target-org ${orgAlias} --json`, {
+        cwd: project_path
+    });
+    const result = JSON.parse(orgFromPackages.toString());
+
+    return result.result.map(x => (
+        {
+            [x.SubscriberPackageName]: {
+                'version': x.SubscriberPackageVersionNumber,
+                'installId': x.SubscriberPackageVersionId,
+            }
+        }
+    ));
 }
+
+console.log(getInstalledPackages(orgFromAlias));
