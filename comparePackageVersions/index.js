3dcc11f4f6e8d3f81215cf1ed8c4ece4e2273028
Sebastian Lech <y2dx@nykredit.dk>
2023-09-14 14:32:23 +0200

    First version of working comparison

--- comparePackageVersions/index.js
+++ comparePackageVersions/index.js
@@ -13,14 +13,30 @@ function getInstalledPackages(orgAlias) {
     });
     const result = JSON.parse(orgFromPackages.toString());
 
-    return result.result.map(x => (
-        {
-            [x.SubscriberPackageName]: {
-                'version': x.SubscriberPackageVersionNumber,
-                'installId': x.SubscriberPackageVersionId,
+    return result.result.reduce(
+        (resultMap, current) => resultMap.set(
+            current.SubscriberPackageName,
+            {
+                'version': current.SubscriberPackageVersionNumber,
+                'installId': current.SubscriberPackageVersionId
             }
-        }
-    ));
+        ),
+        new Map()
+    );
 }
 
-console.log(getInstalledPackages(orgFromAlias));
+const fromOrgPackages = getInstalledPackages(orgFromAlias);
+const toOrgPackages = getInstalledPackages(orgToAlias);
+
+for (const packageName of fromOrgPackages.keys()) {
+    const fromOrgPackage = fromOrgPackages.get(packageName);
+    const toOrgPackage = toOrgPackages.get(packageName);
+    if (!toOrgPackage) {
+        console.log(`package: ${packageName} is not installed on ${orgToAlias}: ${fromOrgPackage.installId}`);
+        continue;
+    }
+    if (fromOrgPackage.version != toOrgPackage.version) {
+        console.log(`${packageName}  ${fromOrgPackage.version} ${toOrgPackage.version}  ${fromOrgPackage.installId}\n`);
+    }
+}
+
