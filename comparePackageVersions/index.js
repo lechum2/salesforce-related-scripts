84e5f49f7bd75947ad6f5a843fc4242cabe8ce2a
Sebastian Lech <x9t@bec.dk>
2025-02-13 10:18:40 +0100

    Install packages that are newer

--- comparePackageVersions/index.js
+++ comparePackageVersions/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const { execSync } = require( 'child_process' );
+const semver = require('semver');
 
 const orgFromAlias = process.argv[2];
 const orgToAlias = process.argv[3];
 
-const project_path = 'C:\\Users\\Y2DX\\workspace\\core';
+const project_path = 'C:\\Users\\x9t\\workspace\\nyk\\core';
+const packagesToSkip = ['bookme', 'NykreditAi'];
 
 function getInstalledPackages(orgAlias) {
     const orgFromPackages = execSync(`sf package installed list --target-org ${orgAlias} --json`, {
@@ -25,6 +27,15 @@ function getInstalledPackages(orgAlias) {
     );
 }
 
+function installPackage(packageName, id, orgAlias) {
+    if (packagesToSkip.includes(packageName)) {
+        return
+    }
+    execSync(`sf package install --package ${id} --wait 20 --target-org ${orgAlias} --security-type AdminsOnly --apex-compile package --no-prompt`, {
+        cwd: project_path
+    });
+}
+
 const fromOrgPackages = getInstalledPackages(orgFromAlias);
 const toOrgPackages = getInstalledPackages(orgToAlias);
 
@@ -33,10 +44,12 @@ for (const packageName of fromOrgPackages.keys()) {
     const toOrgPackage = toOrgPackages.get(packageName);
     if (!toOrgPackage) {
         console.log(`package: ${packageName} is not installed on ${orgToAlias}: ${fromOrgPackage.installId}`);
+        installPackage(packageName, fromOrgPackage.installId, orgToAlias);
         continue;
     }
-    if (fromOrgPackage.version != toOrgPackage.version) {
+    if (semver.gt(semver.coerce(fromOrgPackage.version), semver.coerce(toOrgPackage.version))) {
         console.log(`${packageName}  ${fromOrgPackage.version} ${toOrgPackage.version}  ${fromOrgPackage.installId}\n`);
+        installPackage(packageName, fromOrgPackage.installId, orgToAlias);
     }
 }
 
